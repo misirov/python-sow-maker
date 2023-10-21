18bf9e710681fd08ef7eca921887dc389bfc4733
misirov <carebymedia@gmail.com>
2023-10-21 19:16:11 +0200

    spreadsheet class

---
+++ src/spreadsheet.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+class Spreadsheet:
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    
+    def __init__(self, spreadsheet_id, range_name):
+        self.spreadsheet_id = spreadsheet_id
+        self.range_name = range_name
+        self.service = self.authenticate()
+
+    def authenticate(self):
+        creds = None
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        return build('sheets', 'v4', credentials=creds)
+
+    def fetch_data(self):
+        try:
+            sheet = self.service.spreadsheets()
+            result = sheet.values().get(spreadsheetId=self.spreadsheet_id, range=self.range_name).execute()
+            values = result.get('values', [])
+            if not values:
+                print('No data found.')
+                return []
+            return values
+        except HttpError as err:
+            print(err)
+            return []
+
